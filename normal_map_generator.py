de7eeba2f041b52400ffcd249f2e301486da350c
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-25 12:59:27 +0530

    Convert images to grayscale

--- normal_map_generator.py
+++ normal_map_generator.py
@@ -10,3 +10,13 @@ valid_extensions = ["jpg", "jpeg", "png", "tif", "tiff","targa"]
 image_files = [f for f in os.listdir(input_folder) if f.lower().endswith(valid_extensions)]
 
 print(f"{len(image_files)} images found")
+
+img_dict = {}
+for img_name in image_files:
+    img_path = os.path.join(input_folder, img_name)
+    img=Image.open(img_path).convert("L")
+    img_array = np.array(img)
+
+    img_dict[img_name] = img_array
+    print(f"Loaded {img_name} with size {img_array.shape}")
+
